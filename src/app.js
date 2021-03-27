42b7e238879d15856cfe2cd96bf41c43a7a555b0
Mika C <mcabudol@pamperedchef.com>
2021-03-27 10:52:28 -0500

    clean unneeded repositories + renaming endpoints

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.json({ limit: "16mb" }));
 // ROUTES
-app.get(`/storefront`, storefront_route_1.storefrontRouteHandler);
+app.get(`/product-listing`, storefront_route_1.storefrontRouteHandler);
 app.listen(3037, () => {
     console.log("Now listening at port: " + 3037);
 });
--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import bodyParser = require("body-parser");
 app.use(bodyParser.json({ limit: "16mb" }));
 
 // ROUTES
-app.get(`/storefront`, storefrontRouteHandler);
+app.get(`/product-listing`, storefrontRouteHandler);
 
 app.listen(3037, () => {
     console.log("Now listening at port: " + 3037);

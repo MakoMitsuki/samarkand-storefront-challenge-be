73c9f9cac5d993bd0cc6414cbc4e6fc49558cf8a
Mika C <mcabudol@pamperedchef.com>
2021-03-29 09:46:27 -0500

    more cleanup

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,25 @@
 import * as dotenv from "dotenv";
 
+// import routes
 import { productByIdRouteHandler } from "./routes/product-by-id.route";
 import { productListingRouteHandler } from "./routes/product-listing.route";
 import { productsOnSaleRouteHandler } from "./routes/products-on-sale.route";
 
+// configure express
 dotenv.config();
 import express = require("express");
 const app = express();
 import bodyParser = require("body-parser");
 app.use(bodyParser.json({ limit: "16mb" }));
 
-// ROUTES
+/* ROUTES */
+// /product-listing - grabs entire product listing
 app.get(`/product-listing`, productListingRouteHandler);
+
+// /product-by-id/:id - grabs a product by the product id
 app.get(`/product-by-id/:id`, productByIdRouteHandler);
+
+// /products-on-sale - grabs the products that are on sale
 app.get(`/products-on-sale`, productsOnSaleRouteHandler);
 
 app.listen(process.env.PORT, () => {
--- src/data/fetch-product-db.ts
+++ src/data/fetch-product-db.ts
@@ -1,6 +1,7 @@
 import { Product } from "../types/product";
 import * as db from "./products.json";
 
+// Imports the JSON file and parses it as a list of Products
 export function fetchProductDb(): Product[] {
     const dbParse = (db as any).default;
     return dbParse.products;
